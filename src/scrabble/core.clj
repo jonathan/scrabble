9c0ec616ebc57a039f9502c85c397b7aaa2c8d0a
Jonathan Hicks <jonathan.r.hicks@gmail.com>
2012-07-17 18:36:55 -0400

    Found frequencies. Who knew!

--- src/scrabble/core.clj
+++ src/scrabble/core.clj
@@ -4,24 +4,15 @@
 ; Pulled a lot of this from http://lethain.com/reading-file-in-clojure/
 (defn process-file [file-name line-func line-map]
   (with-open [rdr (BufferedReader. (FileReader. file-name))]
-    (reduce line-func line-map (line-seq rdr))))
+    (line-func line-map (line-seq rdr))))
 
-; Note use of fnil below - returns 0 if nil is passed to inc (avoids null pointer exception)
-(defn char-cnt [s]
-  (reduce
-    (fn [m k]
-      (update-in m [k] (fnil inc 0)))
-  {}
-  (seq s)))
+(defn process-line [letters lines]
+  (filter (fn [line]
+            (= letters (frequencies line)))
+          lines))
 
-(defn process-line [letters line]
-  (if (= letters (char-cnt line))
-    (println ("found" line))
-    (line)))
-
-; Got this from: http://clojuredocs.org/clojure_core/clojure.core/update-in
 (defn main [letters]
-  (process-file "/usr/share/dict/words" process-line (char-cnt letters)))
+  (process-file "/usr/share/dict/words" process-line (frequencies letters)))
 
 (defn -main [& args]
   (if (rest args)
